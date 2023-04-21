e37f2d38140216a080a20169474e32b0640818e3
damienzh <kezh0005@foxmail.com>
2023-04-21 09:06:20 +0800

    update opencv build script

--- ubuntu/opencv/opencv_build.sh
+++ ubuntu/opencv/opencv_build.sh
@@ -27,6 +27,7 @@ cd /tmp
 wget -O opencv.zip https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
 wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip
 unzip opencv.zip && unzip opencv_contrib.zip
+rm opencv.zip opencv_contrib.zip
 mv opencv-${OPENCV_VERSION} opencv && mv opencv_contrib-${OPENCV_VERSION} opencv_contrib
 
 mkdir -p /tmp/build
