121249b957373f8f69b59b4b07e6aa4a2a494bc0
damienzh <kezh0005@foxmail.com>
2023-04-21 08:39:46 +0800

    update scripts

---
+++ ubuntu/opencv/install_opencv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+OPENCV_VERSION=$2
+
+pip3 install opencv-python==$OPENCV_VERSION opencv-contirb-python=$OPENCV_VERSION
--- ubuntu/opencv/opencv_build.sh
+++ ubuntu/opencv/opencv_build.sh
@@ -19,24 +19,50 @@ sudo apt-get install -y libhdf5-dev protobuf-compiler
 sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
 
 # Download opencv and opencv_contrib source
+cd /tmp
 git clone --depth 1 --branch ${OPENCV_VERSION} https://github.com/opencv/opencv.git
 git clone --depth 1 --branch ${OPENCV_VERSION} https://github.com/opencv/opencv_contrib.git
-cd opencv
-mkdir build
-cd build
+
+mkdir -p /tmp/build
+cd /tmp/build
 
 # run cmake
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
--D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
--D WITH_CUDA=ON \
--D OPENCV_DNN_CUDA=ON \
--D CUDA_FAST_MATH=ON \
--D CMAKE_INSTALL_PREFIX=/usr/local \
--D BUILD_opencv_python2=OFF \
--D BUILD_opencv_python3=ON \
--D OPENCV_ENABLE_NONFREE=ON \
-../
+cmake \
+    -D CPACK_BINARY_DEB=ON \
+    -D BUILD_EXAMPLES=OFF \
+    -D BUILD_opencv_python2=OFF \
+    -D BUILD_opencv_python3=ON \
+    -D BUILD_opencv_java=OFF \
+    -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D CUDA_ARCH_BIN=${CUDA_ARCH_BIN} \
+    -D CUDA_ARCH_PTX= \
+    -D CUDA_FAST_MATH=ON \
+    -D CUDNN_INCLUDE_DIR=/usr/include \
+    -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
+    -D WITH_EIGEN=ON \
+    -D ENABLE_NEON=OFF \
+    -D OPENCV_DNN_CUDA=ON \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+    -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D WITH_CUBLAS=ON \
+    -D WITH_CUDA=ON \
+    -D WITH_CUDNN=ON \
+    -D WITH_GSTREAMER=ON \
+    -D WITH_LIBV4L=ON \
+    -D WITH_OPENGL=OFF \
+    -D WITH_OPENCL=OFF \
+    -D WITH_GTK=ON \
+    -D WITH_IPP=OFF \
+    -D WITH_TBB=ON \
+    -D BUILD_TIFF=ON \
+    -D BUILD_PERF_TESTS=OFF \
+    -D BUILD_TESTS=OFF \
+    ../opencv
 
 # build
 make
 sudo make install
+rm -r /tmp/opencv && rm -r /tmp/opencv_contrib
+rm -r /tmp/build
