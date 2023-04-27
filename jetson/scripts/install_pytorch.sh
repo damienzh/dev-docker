2ec368f93e3e44362a064decbee90f11165c0b2a
damienzh <kezh0005@foxmail.com>
2023-04-27 09:58:22 +0800

    [FIX] fix torch wheel name

--- jetson/scripts/install_pytorch.sh
+++ jetson/scripts/install_pytorch.sh
@@ -3,6 +3,7 @@ PYTORCH_VERSION_SUFFIX=$2
 TORCHVISION_VERSION=$3
 TORCHVISION_VERSION_SUFFIX=$4
 PYTORCH_DOWNLOAD_URL=$5
+PYTHON_VERSION=$6
 
 apt-get update && \
 apt-get install -y --no-install-recommends \
@@ -20,9 +21,12 @@ apt-get install -y --no-install-recommends \
 pip3 install --no-cache-dir setuptools Cython wheel
 pip3 install --no-cache-dir --verbose numpy
 
-wget -O torch_$PYTORCH_VERSION.whl $PYTORCH_DOWNLOAD_URL && \
-pip3 install torch_$PYTORCH_VERSION.whl && \
-rm torch_$PYTORCH_VERSION.whl
+PY=${PYTHON_VERSION//"."/""}
+TORCH_WHEEL=torch-$PYTORCH_VERSION-cp$PY-cp$PY-linux_aarch64.whl
+
+wget -O $TORCH_WHEEL $PYTORCH_DOWNLOAD_URL && \
+pip3 install $TORCH_WHEEL && \
+rm $TORCH_WHEEL
 
 TORCH_CUDA_ARCH_LIST="5.3;6.2;7.2;8.7"
 apt-get update && \
